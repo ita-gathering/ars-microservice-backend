d62e9f398bfc567caaab175f1895abe42400adaf
Ocean Liang <ocean.liang@cargosmart.com>
2019-03-22 22:04:35 +0800

    set up parent pom and eureka-server

---
+++ eureka/src/main/java/com/springcloud/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Ocean Liang
+ * @date 3/22/2019
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
