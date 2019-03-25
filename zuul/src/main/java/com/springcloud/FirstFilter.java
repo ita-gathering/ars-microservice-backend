003587bd4cdf76a64c3782b6519d6d704d0f3d1b
Ocean Liang <ocean.liang@cargosmart.com>
2019-03-25 14:14:45 +0800

    solve CORS issue in Zuul

---
+++ zuul/src/main/java/com/springcloud/FirstFilter.java
@@ -0,0 +1,52 @@
+package com.springcloud;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Ocean Liang
+ * @date 3/25/2019
+ */
+@Component
+public class FirstFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        //只过滤OPTIONS 请求
+        return request.getMethod().equals(RequestMethod.OPTIONS.name());
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletResponse response = ctx.getResponse();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT,PATCH");
+        response.setHeader("Access-Control-Expose-Headers", "X-forwared-port, X-forwarded-host");
+        response.setHeader("Vary", "Origin,Access-Control-Request-Method,Access-Control-Request-Headers");
+
+        ctx.setSendZuulResponse(false);
+        ctx.setResponseStatusCode(200);
+        return null;
+    }
+}
